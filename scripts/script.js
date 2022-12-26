c1c38f7dc503a4a14011fb4dfd4c9d6421f07c27
Vsevolod Podtyapurin <sevakuska@outlook.com>
2022-12-27 01:17:00 +0500

    final versoin

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,19 @@
-let buttons = document.querySelectorAll('.nav-btn');
-let articles = document.querySelectorAll('.article');
-
-const changeBlock = function (id) {
-    for (let button of buttons) {
-        if (button.classList.contains('active')) {
-            button.classList.remove('active');
-        }
+const changeColorTheme = function() {
+    element = document.getElementById('cringe');
+    if (element.dataset.bsTheme == 'light') {
+        document.getElementById('logo_img').src = '/images/logo_light.png';
+        element.dataset.bsTheme = 'dark';
+        document.getElementById('change-theme-button').innerText = 'Светлая тема';
+        footer = document.getElementById('footer');
+        footer.classList.remove('bg-dark');
+        footer.classList.add('bg-light');
+        document.getElementById('ithub_ekat_footer_logo').src = '/images/ithub_ekat_dark.png';
+    } else {
+        document.getElementById('logo_img').src = '/images/logo_dark.png';
+        element.dataset.bsTheme = 'light';
+        document.getElementById('change-theme-button').innerText = 'Тёмная тема';
+        footer.classList.remove('bg-light');
+        footer.classList.add('bg-dark');
+        document.getElementById('ithub_ekat_footer_logo').src = '/images/ithub_ekat_light.png';
     }
-    let button = buttons[id];
-    if (button.classList.contains('active') == false) {
-        button.classList.add('active');
-        for (let article of articles) {
-            if (article.classList.contains('non-visible') == false) {
-                article.classList.add('non-visible');
-            }
-        }
-    }
-    let article = articles[id];
-    article.classList.remove('non-visible');
 }
